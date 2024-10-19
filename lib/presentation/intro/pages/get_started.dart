92a3d1b333d6a2a81e3130f9a60528b54f7fce8c
mayank2130 <mayankthakur1712@gmail.com>
2024-10-20 00:30:05 +0530

    choose mode page ui

--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -4,6 +4,7 @@ import 'package:quesso/common/widgets/button/basic_app_button.dart';
 import 'package:quesso/core/configs/assets/app_images.dart';
 import 'package:quesso/core/configs/assets/app_vectors.dart';
 import 'package:quesso/core/configs/theme/app_colors.dart';
+import 'package:quesso/presentation/choose_mode/pages/choose_mode.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -14,16 +15,19 @@ class GetStartedPage extends StatelessWidget {
       body: Stack(
         children: [
           Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: 40,
-              horizontal: 40
-            ),
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
             decoration: const BoxDecoration(
                 image: DecorationImage(
                     fit: BoxFit.fill,
                     image: AssetImage(
                       AppImages.introBG,
                     ))),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
             child: Column(
               children: [
                 Align(
@@ -45,16 +49,23 @@ class GetStartedPage extends StatelessWidget {
                       fontWeight: FontWeight.w500,
                       color: AppColors.grey,
                       fontSize: 15),
-                      textAlign: TextAlign.center,
+                  textAlign: TextAlign.center,
                 ),
-                const SizedBox(height: 20,),
-                BasicAppButton(onPressed: (){}, title: "Get Started")
+                const SizedBox(
+                  height: 20,
+                ),
+                BasicAppButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  const ChooseModePage()));
+                    },
+                    title: "Get Started")
               ],
             ),
           ),
-          Container(
-            color: Colors.black.withOpacity(0.15),
-          )
         ],
       ),
     );
