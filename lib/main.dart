3075d58dd42f2b946eabf0b2a658926f210bdf76
mayank2130 <mayankthakur1712@gmail.com>
2024-10-29 21:07:07 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:quesso/core/configs/theme/app_theme.dart';
+import 'package:quesso/firebase_options.dart';
 import 'package:quesso/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:quesso/presentation/splash/pages/splash.dart';
 
@@ -14,6 +16,9 @@ Future<void> main() async {
         ? HydratedStorage.webStorageDirectory
         : await getApplicationDocumentsDirectory(),
   );  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
